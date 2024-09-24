e44125b777a9ed34dd1612b05022ff08116b257a
Marissa Bell <marissabell@westernwyoming.edu>
2024-09-23 22:12:15 -0700

    Add files via upload

---
+++ m05/helloWorld.js
@@ -0,0 +1,8 @@
+/*
+    Student Name: Marissa Bell
+    File Name: helloWorld.js
+    Date: 09/22/2024
+*/
+
+let hello = "Hello World";
+console.log(hello);
---
+++ m05/practiceVariables.js
@@ -0,0 +1,21 @@
+/*
+    Student Name: Marissa Bell
+    File Name: practiceVariable.js
+    Date: 9/22/24
+*/
+
+let hiDrT = "Hi Dr. T!";
+console.log (hiDrT);
+
+
+let firstNumber = "4.5";
+console.log(firstNumber);
+
+let zero = "0";
+console.log(zero);
+
+let negTen = "-10";
+console.log(negTen);
+
+let t = "true";
+console.log(t);
