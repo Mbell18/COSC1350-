69c91ce0751272ceb92307dfe93e1ba417e45f85
Marissa Bell <marissabell@westernwyoming.edu>
2024-09-28 21:00:34 -0700

    Add files via upload

---
+++ fizzBuzz.js
@@ -0,0 +1,21 @@
+
+
+/*
+    Student Name: Marissa Bell
+    File Name: fizzBuzz.js
+    Date: 9/28/2024
+*/
+
+for(let number = 1; number <=100; number++) {
+
+    if (number % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (number % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(number);
+    }
+    
+}
